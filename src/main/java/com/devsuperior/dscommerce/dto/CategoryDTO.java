dfd01b239df00500beec6eab98e11d25efba5f8b
mmont <mmont@Maycon>
2025-03-07 16:59:51 -0300

    productDTO with Categories

---
+++ src/main/java/com/devsuperior/dscommerce/dto/CategoryDTO.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Category;
+
+public class CategoryDTO {
+
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public CategoryDTO() {
+	}
+
+	public CategoryDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public CategoryDTO(Category category) {
+		id = category.getId();
+		name = category.getName();
+	}
+
+}
